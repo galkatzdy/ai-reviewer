41cec927a388e71f50d36540b5daa74e55196fb5
gal katz <gal.katz@dynamicyield.com>
2025-02-21 13:36:19 +0200

    fix: test

--- reviewer.ts
+++ reviewer.ts
@@ -9,9 +9,9 @@ const test = async () => {
 
   const prompt = `Your job is to only check styling guidelines. You must ignore any code that is not related to styling guidelines mentioned below.
   The guidelines:
-  - Every const variable needs to be declared as lowerCamelCase. One-word variables are allowed.
+  - Every const variable needs to be declared as camelCase. One-word variables are allowed to be in lowercase.
   - Environment variables should be in all caps.
-  - Names of functions MUST be in lowerCamelCase. One-word functions are allowed.
+  - Names of functions MUST be in camelCase. One-word functions are allowed to be in lowercase.
   - A styled component that uses styled-components should not declare the colors manually instead use the theme.
   You have the following code: ${process.env.DIFF}. 
   Your response MUST ALWAYS ONLY output as structured json as follows:
