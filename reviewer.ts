addef07c98792ff040c5a72803858beb1af4bc5c
gal katz <gal.katz@dynamicyield.com>
2025-02-21 11:01:20 +0200

    chore: test reviewer

--- reviewer.ts
+++ reviewer.ts
@@ -2,15 +2,14 @@ import OpenAI from 'openai';
 import core from '@actions/core';
 
 const test = async () => {
-  // change base URL
   const openai = new OpenAI({
-    apiKey: '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',
+    apiKey: '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',
     baseURL: 'https://openrouter.ai/api/v1',
   });
 
   const result = await openai.chat.completions.create({
     model: 'google/gemini-2.0-flash-lite-preview-02-05:free',
-    messages: [{ role: 'user', content: 'hello' }],
+    messages: [{ role: 'user', content: 'test' }],
   });
 
   core.setOutput('review', result.choices[0].message.content);
