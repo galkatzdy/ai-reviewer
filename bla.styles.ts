0c557e9b43913c4355f270b79365297966970073
gal katz <gal.katz@dynamicyield.com>
2025-02-21 13:28:54 +0200

    fix: test

--- bla.styles.ts
+++ bla.styles.ts
@@ -2,5 +2,5 @@ import { Button } from '@material-ui/core';
 import styled from 'styled-components';
 
 export const StyledButton = styled(Button)`
-  color: ${({ theme }) => theme.colors.red};
+  color: red;
 `;
