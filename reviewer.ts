b122c6821c9c04333e562c6ac66e4add7a3cbe3a
gal katz <gal.katz@dynamicyield.com>
2025-02-21 11:58:30 +0200

    chore: test reviewer

--- reviewer.ts
+++ reviewer.ts
@@ -6,11 +6,19 @@ const test = async () => {
     apiKey: process.env.API_KEY,
     baseURL: 'https://openrouter.ai/api/v1',
   });
+  // At the beginning of the response please mention the file you are reviewing. Also attach a code fix with the relevant context.
   const prompt = `Your job is to only check styling guidelines.
-  At the beginning of the response please mention the file you are reviewing. Also attach a code fix with the relevant context. 
   The guidelines:
-  - Every variable needs to be declared as camelCase. Doesn't apply to: one word variables, names, environment variables.
-  You the following code: ${process.env.DIFF}. If you don't find anything wrong, please return a string of 'NO_ISSUES'.`;
+  - Every variable needs to be declared as camelCase.
+  - Environment variables should be in all caps.
+  - One word variables should be in lowercase.
+  - Names of functions should be in camelCase.
+  You the following code: ${process.env.DIFF}. Your output json should be structured as follows:
+  [{
+    "file": <filename>,
+    "fixedCode": <fixed code> | 'NO_ISSUE',
+  }]
+  `;
 
   const result = await openai.chat.completions.create({
     model: 'google/gemini-2.0-pro-exp-02-05:free',
