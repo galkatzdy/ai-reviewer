c74908ab424a14265f1e4c2527ee0ca7936401db
gal katz <gal.katz@dynamicyield.com>
2025-02-21 11:16:33 +0200

    chore: test reviewer

--- reviewer.ts
+++ reviewer.ts
@@ -6,10 +6,11 @@ const test = async () => {
     apiKey: process.env.API_KEY,
     baseURL: 'https://openrouter.ai/api/v1',
   });
+  const prompt = `You the following code: ${process.env.DIFF}. Please find bugs in the code. If you don't find anything you don't have to respond.`;
 
   const result = await openai.chat.completions.create({
     model: 'google/gemini-2.0-flash-lite-preview-02-05:free',
-    messages: [{ role: 'user', content: 'test' }],
+    messages: [{ role: 'user', content: prompt }],
   });
 
   core.setOutput('review', result.choices[0].message.content);
