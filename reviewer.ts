58523cebb7dd8c8db298c166c735d14f3e3c1f8f
gal katz <gal.katz@dynamicyield.com>
2025-02-21 11:54:20 +0200

    chore: test reviewer

--- reviewer.ts
+++ reviewer.ts
@@ -9,7 +9,7 @@ const test = async () => {
   const prompt = `Your job is to only check styling guidelines.
   At the beginning of the response please mention the file you are reviewing. Also attach a code fix with the relevant context. 
   The guidelines:
-  - Every variable needs to be declared as camelCase. Doesn't apply to one word variables or names.
+  - Every variable needs to be declared as camelCase. Doesn't apply to: one word variables, names, environment variables.
   You the following code: ${process.env.DIFF}. If you don't find anything wrong, please return a string of 'NO_ISSUES'.`;
 
   const result = await openai.chat.completions.create({
